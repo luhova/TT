0ac525c2f263f740333b55e82cc7fd37c6e54bb5
Gabriela Luhova <gabi@tutuf.com>
2015-07-13 17:13:07 +0300

    Iniital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tt::Application.config.secret_token = '14b9908dd0425702f3dceeaaa1b77e724cc48378d4e88e92bf79570f9df9aaad63621ddaca114d697ead912a4f6b5a206b106c7a37b535f4260236da8d041154'
