d86440edcab92a80606f4e85014e3e97fb3ab480
Gabriela Luhova <gabi@tutuf.com>
2015-07-15 13:52:33 +0300

    Add create, index and show action for the TimeTablesController

--- app/controllers/time_tables_controller.rb
+++ app/controllers/time_tables_controller.rb
@@ -1,4 +1,39 @@
 class TimeTablesController < ApplicationController
   def new
   end
+
+  def create
+     @time_table = TimeTable.new(params[:time_table])
+
+     respond_to do |format|
+      if @time_table.save
+        format.html  { redirect_to(@time_table,
+                      :notice => 'Time-table was successfully created.') }
+        format.json  { render :json => @time_table,
+                      :status => :created, :location => @time_table }
+      else
+        format.html  { render :action => "new" }
+        format.json  { render :json => @time_table.errors,
+                      :status => :unprocessable_entity }
+      end
+    end
+  end
+
+  def index
+    @time_tables = TimeTable.all
+
+    respond_to do |format|
+      format.html  # index.html.erb
+      format.json  { render :json => @time_tables }
+    end
+  end
+
+  def show
+    @time_table = TimeTable.find(params[:id])
+
+    respond_to do |format|
+      format.html  # show.html.erb
+      format.json  { render :json => @time_table }
+    end
+  end
 end
