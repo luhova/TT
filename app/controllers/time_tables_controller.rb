7a1af10fe9d7e0b09881ec09cf47f30e246d9c47
Gabriela Luhova <gabi@tutuf.com>
2015-07-15 16:00:21 +0300

    Add edit action for the TimeTablesController

--- app/controllers/time_tables_controller.rb
+++ app/controllers/time_tables_controller.rb
@@ -36,4 +36,24 @@ class TimeTablesController < ApplicationController
       format.json  { render :json => @time_table }
     end
   end
+
+  def edit
+    @time_table = TimeTable.find(params[:id])
+  end
+
+  def update
+  @time_table = TimeTable.find(params[:id])
+
+  respond_to do |format|
+    if @time_table.update_attributes(params[:time_table])
+      format.html  { redirect_to(@time_table,
+                    :notice => 'Time-table was successfully updated.') }
+      format.json  { head :no_content }
+    else
+      format.html  { render :action => "edit" }
+      format.json  { render :json => @time_table.errors,
+                    :status => :unprocessable_entity }
+    end
+  end
+end
 end
