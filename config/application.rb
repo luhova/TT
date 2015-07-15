f8d8c810032b01c8942c29dcadd738011b9d2b76
Gabriela Luhova <gabi@tutuf.com>
2015-07-15 16:44:44 +0300

    Change time zone

--- config/application.rb
+++ config/application.rb
@@ -36,6 +36,8 @@ module Tt
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
 
+    config.time_zone = "Sofia"
+
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
 
