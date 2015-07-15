dfa0a019295e473b2944a28779abf0a778c810fa
Gabriela Luhova <gabi@tutuf.com>
2015-07-15 13:32:22 +0300

    Add a home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Tt::Application.routes.draw do
 
+  get "home/index"
+
   resources :time_tables
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -50,7 +52,7 @@ Tt::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'home#index'
 
   # See how all your routes lay out with "rake routes"
 
