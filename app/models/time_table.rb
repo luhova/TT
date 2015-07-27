2e9ec67e83349a1b1b063cf946a4995d01aa7062
Gabriela Luhova <gabi@tutuf.com>
2015-07-27 14:14:47 +0300

    Add validations

--- app/models/time_table.rb
+++ app/models/time_table.rb
@@ -1,6 +1,8 @@
 class TimeTable < ActiveRecord::Base
   attr_accessible :from, :time, :to, :transport
 
+  validates :from, :time, :to, :transport, presence: true
+
   def self.search(search_from, search_to)
     TimeTable.where({from: search_from, to: search_to})
   end
