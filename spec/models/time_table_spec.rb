94d6605fa4505d97a04f4ff6f15eb766c9a1c35f
Gabriela Luhova <gabi@tutuf.com>
2015-07-29 14:51:25 +0300

    Add specs for the TimeTable model

--- spec/models/time_table_spec.rb
+++ spec/models/time_table_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe TimeTable, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it "is invalid without a starting point" do
+    expect{ FactoryGirl.create(:time_table, from: nil) }.to raise_error(ActiveRecord::RecordInvalid)
+  end
+
+  it "is invalid without an ending point" do
+    expect{ FactoryGirl.create(:time_table, to: nil) }.to raise_error(ActiveRecord::RecordInvalid)
+  end
+
+  it "is invalid without time of departure" do
+    expect{ FactoryGirl.create(:time_table, time: nil) }.to raise_error(ActiveRecord::RecordInvalid)
+  end
+
+  it "is invalid without type of transport" do
+    expect { FactoryGirl.create(:time_table, transport: nil) }.to raise_error(ActiveRecord::RecordInvalid)
+  end
+
+  let!(:tt) { FactoryGirl.create(:time_table, from: 'Sofia', to: 'Samokov')}
+
+  describe '.search' do
+    it "returns the right time-table" do
+      expect(TimeTable.find_by_from_and_to('Sofia', 'Samokov')).to eq(tt)
+    end
+  end
+
 end
