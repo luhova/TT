c0db9806e4f890e8050c3bd7ed83ea89c834f0a2
Gabriela Luhova <gabi@tutuf.com>
2015-07-30 18:06:35 +0300

    Add factory for an invalid time-table

--- spec/factories/time-tables.rb
+++ spec/factories/time-tables.rb
@@ -7,4 +7,8 @@ FactoryGirl.define do
     f.time { Faker::Time.between(DateTime.now - 1, DateTime.now) }
     f.transport { Faker::Lorem.word }
   end
+
+  factory :invalid_time_table, parent: :time_table do |f|
+    f.from nil
+  end
 end
